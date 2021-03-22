ca0f54a7ff9e893a0054c3e7f36333371a8b48b4
Алексей Полоз <polozhev@mail.ru>
2021-03-23 01:15:19 +0300

    Get current tasks queue

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import time
 import threading
 import queue
+import typing
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 
-app = FastAPI(title="Task for Elastoo")
+app = FastAPI(title="Elastoo task")
 queue_in = queue.Queue()
 queue_out = queue.Queue()
 
@@ -21,6 +22,17 @@ class TaskOutput(BaseModel):
 class ResultOutput(BaseModel):
 	result: list[int]
 
+class ExampleItem(typing.TypedDict):
+	id: int
+	time: float
+	num: int
+	timeout: float
+
+class QueueOutput(BaseModel):
+	result: list[dict] = [ExampleItem(
+		id=1, time=1616450789.260913, num=5, timeout=0.5,
+	)]
+
 
 def worker():
 	while True:
@@ -28,11 +40,11 @@ def worker():
 			continue
 
 		item = queue_in.queue[0]
-		print(f'Working on {item.num}')
-		time.sleep(item.timeout)
-		print(f'Finished {item.num}')
+		print('Working on {} (ID {})'.format(item['num'], item['id']))
+		time.sleep(item['timeout'])
+		print('Finished {} (ID {})'.format(item['num'], item['id']))
 
-		queue_out.put(item.num)
+		queue_out.put(item['num'])
 		queue_in.get()
 		queue_in.task_done()
 
@@ -45,8 +57,19 @@ threading.Thread(target=worker, daemon=True).start()
 	response_model=TaskOutput,
 )
 async def add(task: TaskInput):
-	queue_in.put(task)
-	return {"position": queue_in.qsize()}
+	history_position = queue_out.qsize()
+	position = queue_in.qsize()+1
+
+	item = {
+		'id': history_position+position,
+		'time': time.time(),
+		'num': task.num,
+		'timeout': task.timeout,
+	}
+
+	queue_in.put(item)
+
+	return {"position": position}
 
 @app.get(
 	"/results",
@@ -55,3 +78,11 @@ async def add(task: TaskInput):
 )
 async def result():
 	return {"result": queue_out.queue}
+
+@app.get(
+	"/tasks",
+	response_description="Current task queue",
+	response_model=QueueOutput,
+)
+async def get():
+	return {"result": queue_in.queue}
